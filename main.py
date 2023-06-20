57fb1c5b960130684b4f53661e178b45b3474c65
Mateo <mateovaldivia3000@gmail.com>
2023-06-20 00:15:39 -0400

    fixed recover path bug and added the input

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ with open('entrada.txt', 'r') as archivo:
         a, b, c ,d = archivo.readline().split()
         a, b, c = int(a), int(b), int(c)
         G[a].append(((b, c), d))
+        G[b].append(((a, c), d))
 
 diccionarioNombres = {}
 with open('nombres.txt', 'r') as archivo2:
@@ -18,7 +19,11 @@ with open('nombres.txt', 'r') as archivo2:
         numeros = list (map(int, valores[2:]))
         diccionarioNombres[texto] = (n, numeros)
 
-def dijsktra(inicio,destino):
+def dijsktra(nombreInicio,nombreDestino):
+    inicio = diccionarioNombres[nombreInicio][1][0]
+    destino = diccionarioNombres[nombreDestino][1][0]
+
+    print("inicio: ",inicio,"destino: ",destino)
     distancia = [float('inf') for _ in range(cantidad_nodos+5)]
     distancia[inicio] = 0
     
@@ -37,24 +42,32 @@ def dijsktra(inicio,destino):
         vis[nodo] = True
         for adyacente in G[nodo]:
             vecino, peso = adyacente[0]
-            nombreCalle[vecino] = adyacente[1]
-            distanciaRecorrida[vecino] = peso
             #print ("nodo: ",nodo,"vecino: ",vecino,"peso: ",peso,"nombreCalle: ",nombreCalle[vecino])
             if distancia[nodo] + peso < distancia[vecino]:
                 distancia[vecino] = distancia[nodo] + peso
                 padre[vecino] = nodo
+                nombreCalle[vecino] = adyacente[1]
+                distanciaRecorrida[vecino] = peso
                 heappush(cola, (distancia[vecino], vecino))
     
     path = []
     nodo = destino
     while nodo!=inicio:
+        print("nodo  ",nodo)
         path.append((nombreCalle[nodo], distanciaRecorrida[nodo]))
         nodo = padre[nodo]
+    print("nodo  ",nodo)
     path.reverse()
     return (distancia[destino], path)
 
 
-dis1, path1 = dijsktra(1, 3)
+dis1, path1 = dijsktra("OVERTIME","SUPERMARKET")
+
 print("la distancia mas corta es:", dis1)
-print("el tiempo estimado para recorrer es: ", dis1*6, " segundos")
+#el tiempo en segundos es 6 segundos por cada metro, quiero pasarlo a horas, minutos y segundo
+hora = dis1*6//3600
+minuto = (dis1*6%3600)//60
+segundo = (dis1*6%3600)%60
+print("el tiempo estimado para recorrer es: ", hora, " horas ", minuto, " minutos ", segundo, " segundos")
+#print("el tiempo estimado para recorrer es: ", dis1*6, " segundos")
 print("el camino mas corto es: ", path1)
